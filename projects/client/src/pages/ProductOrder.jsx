8ff7f70b829cba8d5b201e2dd1628a711f87ddbc
VirgiawanLR <116149015+VirgiawanLR@users.noreply.github.com>
2023-06-05 14:13:16 +0700

    initializing admin navbar

---
+++ projects/client/src/pages/ProductOrder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutAdmin from "../components/LayoutAdmin";
+
+function ProductOrder() {
+  return (
+    <LayoutAdmin>
+      <h1>Product Order</h1>
+    </LayoutAdmin>
+  );
+}
+
+export default ProductOrder;
---
+++ projects/client/src/pages/UserManagement.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutAdmin from "../components/LayoutAdmin";
+
+function UserManagement() {
+  return (
+    <LayoutAdmin>
+      <h1>User Management</h1>
+    </LayoutAdmin>
+  );
+}
+
+export default UserManagement;
