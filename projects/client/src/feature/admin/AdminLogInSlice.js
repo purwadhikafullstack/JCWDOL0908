f1b5057fc46b4b96bc0cba81a56c81ccba3bfa8c
Aditya Rizki Pratama <arizki717@gmail.com>
2023-06-14 12:29:51 +0700

    FIX : "uncomment alert message"

--- projects/client/src/feature/admin/AdminLogInSlice.js
+++ projects/client/src/feature/admin/AdminLogInSlice.js
@@ -36,10 +36,6 @@ export function loggingInAdmin(username, password) {
   };
 }
 
-/**
- * TODO: this keepLoggedIn function is not working properly
- * condition: the error message alert is showing up need to fix it
- */
 export function keepAdminLoggedIn() {
   return async (dispatch) => {
     try {
@@ -47,7 +43,7 @@ export function keepAdminLoggedIn() {
       await dispatch(setLoggedInAdminData({ ...response.data.result, isLoggedIn: true }));
       await localStorage.setItem("admin_token", response.data.token);
     } catch (error) {
-      // alert(error.response.data.message);
+      alert(error.response.data.message);
     }
   };
 }
