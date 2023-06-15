380f8738de0318637e25227397cf5948b75a6bf1
Aditya Rizki Pratama <arizki717@gmail.com>
2023-06-15 20:13:17 +0700

    FIX: "user middleware with try catch"
    Avoid error split

--- projects/server/src/middleware/UserMiddleware.js
+++ projects/server/src/middleware/UserMiddleware.js
@@ -1,16 +1,23 @@
 const { DecodeJWT } = require("../helper/Token");
 
 const CheckAuth = async (req, res, next) => {
-  // Get auth from bearer token
-  const { data, error } = await DecodeJWT(req.headers.authorization.split(" ")[1]);
-  if (error) {
+  try {
+    // Get auth from bearer token
+    const { data, error } = await DecodeJWT(req.headers.authorization.split(" ")[1]);
+    if (error) {
+      return res.status(401).json({
+        message: "Unauthorized",
+        data: null,
+      });
+    }
+    req.user = data;
+    next();
+  } catch (error) {
     return res.status(401).json({
       message: "Unauthorized",
       data: null,
     });
   }
-  req.user = data;
-  next();
 };
 
 module.exports = {
