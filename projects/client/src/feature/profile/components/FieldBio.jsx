31d423d7617a586408637b51702541e116cdc0f0
Aditya Rizki Pratama <arizki717@gmail.com>
2023-06-15 21:44:00 +0700

    FEAT : " api call and handle submit update bio"
    Add APi call for handle submit and add validation phonenumber

--- projects/client/src/feature/profile/components/FieldBio.jsx
+++ projects/client/src/feature/profile/components/FieldBio.jsx
@@ -4,16 +4,18 @@ function FieldBio({ label, ...props }) {
   const [field, meta] = useField(props);
 
   return (
-    <div className="flex flex-col sm:flex-row items-center gap-3">
-      <label className="sm:w-1/4 w-full">{label}</label>
-      <input
-        {...field}
-        {...props}
-        // class name from tailwindcss is disabled or active
-        className={`sm:w-3/4 w-full text-primary font-body border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent disabled:bg-none disabled:border-none disabled:cursor-not-allowed
+    <div>
+      <div className="flex flex-col sm:flex-row items-center gap-3">
+        <label className="sm:w-1/4 w-full">{label}</label>
+        <input
+          {...field}
+          {...props}
+          // class name from tailwindcss is disabled or active
+          className={`sm:w-3/4 w-full text-primary font-body border border-gray-300 rounded-lg py-2 px-4 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent disabled:bg-none disabled:border-none disabled:cursor-not-allowed
         ${meta.touched && meta.error ? "border-red-500" : ""}`}
-      />
-      {meta.touched && meta.error ? <p className="text-red-500 text-sm">{meta.error}</p> : null}
+        />
+      </div>
+      {meta.touched && meta.error ? <p className="text-red-500 text-sm text-center py-1">{meta.error}</p> : null}
     </div>
   );
 }
--- projects/client/src/pages/client/profile/Profile.jsx
+++ projects/client/src/pages/client/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import LayoutClient from "../../../components/LayoutClient";
 import Jumbotron from "../../../components/Jumbotron";
 import ProfileContainer from "../../../feature/profile/components/ProfileContainer";
@@ -7,15 +7,33 @@ import FieldBio from "../../../feature/profile/components/FieldBio";
 import { Form, Formik } from "formik";
 import { useState } from "react";
 import PhotoForm from "../../../feature/profile/components/PhotoForm";
+import { UpdateBio } from "../../../feature/profile/";
+import { setLoading } from "../../../feature/LoaderSlice";
+import { ToastError, ToastSuccess } from "../../../helper/Toastify";
+import { BioValidation } from "../../../validation/User";
 
 function Profile() {
   const { user } = useSelector((state) => state.user);
   const [isUpdate, setIsUpdate] = useState(false);
+  const dispatch = useDispatch();
 
   // protected route
   if (!user?.id) {
     return <h1>Forbidden</h1>;
   }
+
+  const handleSubmit = async (values) => {
+    dispatch(setLoading(true));
+    try {
+      const res = await UpdateBio(values);
+      dispatch(setLoading(false));
+      ToastSuccess(res.data.message || "Update Bio Data Success");
+      setIsUpdate(false);
+    } catch (error) {
+      dispatch(setLoading(false));
+      ToastError(error.response.data.message || "Update Bio Data Failed");
+    }
+  };
 
   return (
     <LayoutClient>
@@ -28,9 +46,9 @@ function Profile() {
             <PhotoForm user={user} />
 
             <Formik
-              initialValues={{ username: user?.username || "", email: user?.email, phone: user?.phone || "" }}
-              onSubmit={() => {
-              }}
+              initialValues={{ username: user?.username || "", email: user?.email, phone: user?.phone_number || "" }}
+              onSubmit={handleSubmit}
+              validationSchema={BioValidation}
             >
               <Form className="flex flex-col gap-5">
                 <FieldBio label="Username" name="username" type="text" disabled={!isUpdate} />
