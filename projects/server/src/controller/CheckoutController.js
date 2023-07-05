7b844d25e871ccc848825843ecfd082e4c2428dc
Aditya Rizki Pratama <arizki717@gmail.com>
2023-07-05 14:31:48 +0700

    FEAT : "server route get and send payment prof"

--- projects/server/src/controller/CheckoutController.js
+++ projects/server/src/controller/CheckoutController.js
@@ -1,5 +1,6 @@
 const { CheckoutService } = require("../service");
 const { CheckoutValidation } = require("../validation");
+const { UploadPhoto } = require("../helper/Multer");
 const checkShippingCoast = async (req, res, next) => {
   try {
     const { body, user } = req;
@@ -67,7 +68,71 @@ const createOrder = async (req, res, next) => {
   }
 };
 
+const payOrder = async (req, res, next) => {
+  try {
+    const upload = await UploadPhoto("transactions");
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(400).send({
+          message: err.message,
+          data: null,
+        });
+      }
+      const transaction_image = req.uniqueUrl;
+      const { id } = req.params;
+
+      const { error, data } = await CheckoutService.storePaymentProof({
+        id_transaction: id,
+        payment_proof: transaction_image,
+      });
+
+      if (error) {
+        return res.status(400).json({
+          message: error.message,
+          data: null,
+        });
+      }
+
+      return res.status(200).json({
+        message: "Payment proof stored successfully",
+        data,
+      });
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getOrders = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const { limit = 6, page = 1 } = req.query;
+
+    const { error, data } = await CheckoutService.getTransactions({
+      id_user: user.id,
+      limit,
+      page,
+    });
+
+    if (error) {
+      return res.status(400).json({
+        message: error.message,
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Orders retrieved successfully",
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   checkShippingCoast,
   createOrder,
+  payOrder,
+  getOrders,
 };
--- projects/server/src/router/CheckoutRouter.js
+++ projects/server/src/router/CheckoutRouter.js
@@ -5,6 +5,8 @@ const { CheckAuth } = require("../middleware/UserMiddleware");
 
 router.post("/shipping-cost", CheckAuth, CheckoutController.checkShippingCoast);
 router.post("/", CheckAuth, CheckoutController.createOrder);
+router.get("/history", CheckAuth, CheckoutController.getOrders);
+router.patch("/payment/:id", CheckAuth, CheckoutController.payOrder);
 
 
 module.exports = router;
